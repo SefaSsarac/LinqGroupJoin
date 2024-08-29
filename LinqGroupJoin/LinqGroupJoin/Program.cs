d729aa1a9fd8176bedb64f22a029e949aaa5f23a
Sefa Saraç <srcsfysf@gmail.com>
2024-08-29 18:22:33 +0300

    Add files via upload

---
+++ LinqGroupJoin/LinqGroupJoin/Program.cs
@@ -0,0 +1,45 @@
+﻿using LinqGroupJoin;
+
+class Program
+{
+    static void Main()
+    {
+        // List of students
+        List<Student> students = new List<Student>
+        {
+            new Student { StudentId = 1, StudentName = "Ali", ClassId = 1 },
+            new Student { StudentId = 2, StudentName = "Ayşe", ClassId = 2 },
+            new Student { StudentId = 3, StudentName = "Mehmet", ClassId = 1 },
+            new Student { StudentId = 4, StudentName = "Fatma", ClassId = 3 },
+            new Student { StudentId = 5, StudentName = "Ahmet", ClassId = 2 }
+        };
+
+        // List of classes
+        List<Class> classes = new List<Class>
+        {
+            new Class { ClassId = 1, ClassName = "Matematik" },
+            new Class { ClassId = 2, ClassName = "Türkçe" },
+            new Class { ClassId = 3, ClassName = "Kimya" }
+        };
+
+        // LINQ Group Join to list students under their respective classes
+        var groupJoinResult = from cls in classes
+                              join stu in students
+                              on cls.ClassId equals stu.ClassId into studentGroup
+                              select new
+                              {
+                                  ClassName = cls.ClassName,
+                                  Students = studentGroup
+                              };
+
+        // Display the result
+        foreach (var cls in groupJoinResult)
+        {
+            Console.WriteLine($"Class: {cls.ClassName}");
+            foreach (var student in cls.Students)
+            {
+                Console.WriteLine($" - {student.StudentName}");
+            }
+        }
+    }
+}
